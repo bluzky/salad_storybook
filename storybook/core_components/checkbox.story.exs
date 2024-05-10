4836cbb6409f57c93e6806a87b04e3bc0be44a7a
Dzung Nguyen <bluesky.1289@gmail.com>
2024-05-10 17:26:46 +0700

    Add checkbox and label

---
+++ storybook/core_components/checkbox.story.exs
@@ -0,0 +1,32 @@
+defmodule Storybook.CoreComponents.Checkbox do
+  use PhoenixStorybook.Story, :component
+
+  def function, do: &SaladUI.Checkbox.checkbox/1
+  def imports, do: [{SaladUI.Label, [{:label, 1}]}]
+
+  def template do
+    """
+    <div className="flex items-center space-x-2">
+      <.psb-variation/>
+      <.label>I'm a label</.label>
+    </div>
+    """
+  end
+
+  def variations do
+    [
+      %Variation{
+        id: :checkbox_checked,
+        attributes: %{
+          value: true
+        }
+      },
+      %Variation{
+        id: :checkbox,
+        attributes: %{
+          checked: false
+        }
+      }
+    ]
+  end
+end
