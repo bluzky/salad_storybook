5d0a0bb6b870255d5b7d95b7c14aa457a25f9161
Dzung Nguyen <bluesky.1289@gmail.com>
2024-05-29 11:14:10 +0700

    Fix welcome

--- storybook/salad_ui_component/select.story.exs
+++ storybook/salad_ui_component/select.story.exs
@@ -6,14 +6,52 @@ defmodule Storybook.SaladUIComponents.Select do
 
   def function, do: &SaladUI.Select.select/1
 
+  def imports,
+    do: [
+      {SaladUI.Select,
+       [
+         select_trigger: 1,
+         select_content: 1,
+         select_group: 1,
+         select_label: 1,
+         select_item: 1,
+         select_value: 1,
+         select_separator: 1
+       ]},
+      {SaladUI.Button, [button: 1]}
+    ]
+
   def variations do
     [
       %Variation{
         id: :select,
         attributes: %{
-          prompt: "select your color",
-          options: ["Red", "Green", "White", "Yellow"]
-        }
+          name: "fruit"
+        },
+        template: """
+        <form>
+          <.psb-variation />
+          <.button type="submit" class="w-full mt-2">Submit</.button>
+        </form>
+        """,
+        slots: [
+          """
+            <.select_trigger target="my-select" class="w-[180px]">
+              <.select_value placeholder=".select a fruit" />
+            </.select_trigger>
+            <.select_content id="my-select" class="w-full">
+              <.select_group>
+                <.select_label>Fruits</.select_label>
+                <.select_item name="fruit" value="apple">Apple</.select_item>
+                <.select_item name="fruit" value="banana">Banana</.select_item>
+                <.select_item name="fruit" selected value="blueberry">Blueberry</.select_item>
+                <.select_separator />
+                <.select_item name="fruit" disabled value="grapes">Grapes</.select_item>
+                <.select_item name="fruit" value="pineapple">Pineapple</.select_item>
+              </.select_group>
+            </.select_content>
+          """
+        ]
       }
     ]
   end
--- storybook/welcome.story.exs
+++ storybook/welcome.story.exs
@@ -27,8 +27,8 @@ defmodule Storybook.MyPage do
         <a
           target="_blank"
           rel="noreferrer"
-          class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2"
-          href="https://github.com/shadcn-ui/ui"
+          class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors text-white border border-input bg-primary shadow-sm h-9 px-4 py-2"
+          href="https://github.com/bluzky/salad_ui"
         >
           <Lucideicons.github class="h-4 w-4 mr-2" />Visit GitHub
         </a>
