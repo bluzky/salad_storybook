3b9e0b43a56378e7d856b8daaa7bc4e3ea7b0432
Dzung Nguyen <bluesky.1289@gmail.com>
2024-06-03 21:03:56 +0700

    Add story for progress bar

---
+++ storybook/salad_ui_component/progress.story.exs
@@ -0,0 +1,27 @@
+defmodule Storybook.SaladUIComponents.Progress do
+  @moduledoc false
+  use PhoenixStorybook.Story, :component
+
+  def function, do: &SaladUI.Progress.progress/1
+
+
+  def variations do
+    [
+      %Variation{
+        id: :progress_bar,
+        attributes: %{
+          value: 20,
+          class: "w-[60%]"
+        }
+      },
+            %Variation{
+        id: :thin_progress_bar,
+        attributes: %{
+          value: 20,
+          class: "w-[60%] h-2"
+        }
+      }
+
+    ]
+  end
+end
