5d515d925883b8edca196050799eaf2d36b37079
Dzung Nguyen <bluesky.1289@gmail.com>
2024-06-25 21:38:54 +0700

    Update checkbox story

--- storybook/salad_ui_component/checkbox.story.exs
+++ storybook/salad_ui_component/checkbox.story.exs
@@ -5,28 +5,28 @@ defmodule Storybook.SaladUIComponents.Checkbox do
   def function, do: &SaladUI.Checkbox.checkbox/1
   def imports, do: [{SaladUI.Label, [{:label, 1}]}]
 
-  def template do
-    """
-    <div className="flex items-center space-x-2">
-      <.psb-variation/>
-      <.label>I'm a label</.label>
-    </div>
-    """
-  end
-
   def variations do
     [
       %Variation{
         id: :checkbox_checked,
+        template: """
+        <div className="flex items-center space-x-2">
+        <.checkbox id="checked" value={true}/>
+        <.label for="checked">I'm a label</.label>
+        </div>
+        """,
         attributes: %{
           value: true
         }
       },
       %Variation{
         id: :checkbox,
-        attributes: %{
-          checked: false
-        }
+        template: """
+        <div className="flex items-center space-x-2">
+        <.checkbox id="unchecked"/>
+        <.label for="unchecked">I'm a label</.label>
+        </div>
+        """
       }
     ]
   end
