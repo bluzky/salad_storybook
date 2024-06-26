1531996cd5e9bbf1de9f6b5126a59d1a95a32043
Dzung Nguyen <bluesky.1289@gmail.com>
2024-06-26 14:14:41 +0700

    Update deps

--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule SaladStorybook.MixProject do
 
       # load local salad_ui on dev from local path
       (Mix.env() == :prod &&
-         {:salad_ui, "~> 0.5.1"}) ||
+         {:salad_ui, github: "bluzky/salad_ui", tag: "v0.5.2"}) ||
         {:salad_ui, path: "../salad_ui"}
     ]
   end
